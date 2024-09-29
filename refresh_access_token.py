f534b6daf149ad0e9c26604caa99e6b266b13469
Jesse Chen <jessechen@meta.com>
2024-09-29 14:00:16 -0700

    create script to refresh access token

---
+++ refresh_access_token.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+from dotenv import dotenv_values
+
+config = dotenv_values(".env.local")
+
+TH_ACCESS_TOKEN = config['THREADS_ACCESS_TOKEN']
+
+try:
+  result = urlopen('https://graph.threads.net/refresh_access_token?grant_type=th_refresh_token&access_token={access_token}'
+    .format(access_token=TH_ACCESS_TOKEN)).read()
+except HTTPError as e:
+  print('Error code: ', e.code)
+except URLError as e:
+  print('Reason: ', e.reason)
+else:
+  print('IG access token renewed!')
+  print('\n === RESPONSE === \n')
+  print(result)
